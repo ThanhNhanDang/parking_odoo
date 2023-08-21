3783fcb4294ea5abdd1484bbfc46d5cf6e33f18e
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-21 11:17:27 +0700

    add api /parking/get/move_history/get_all/epc

--- controllers/controllers_move_history.py
+++ controllers/controllers_move_history.py
@@ -6,10 +6,9 @@ class ControllerMoveHistory(http.Controller):
 
     @http.route('/parking/get/move_history/get_all/epc', website=False,csrf=False, type='json', methods=['POST'], auth='public')
     def get_all_move_history(self, **kw):
-        move_histories = http.request.env['stock.move.line'].sudo().read_group(
-            domain=[('lot_name', '=', kw['sEPC'])],
+        move_histories = http.request.env['stock.move.line'].sudo().search_read(
+            domain=[('lot_name', '=', kw['sEPC']), ('create_date','=',fields.datetime.now())],
             fields=['date','lot_name', 'reference','location_id','location_dest_id'],
-            groupby=['date:day']
         )
         return move_histories
 
