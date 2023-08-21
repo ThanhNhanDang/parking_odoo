10c916d7328b40a35bf3c3d40aef2f0f0ba051cd
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-21 11:36:35 +0700

    add api /parking/get/move_history/get_all/epc

--- controllers/controllers_move_history.py
+++ controllers/controllers_move_history.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo import fields
+from datetime import date
+
 class ControllerMoveHistory(http.Controller):
    
 
@@ -10,6 +12,11 @@ class ControllerMoveHistory(http.Controller):
             domain=[('lot_name', '=', kw['sEPC'])],
             fields=['date','lot_name', 'reference','location_id','location_dest_id'],
         )
+        today = date.today()
+        for move_history in move_histories: 
+            if move_history.date.split(" ")[0] != today:
+                move_histories.pop(move_history)
+                
         return move_histories
 
    
