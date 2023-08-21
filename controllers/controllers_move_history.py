73192cfeafcf151db6cc4ee54f7a743d1c07a50e
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-21 10:37:47 +0700

    add api /parking/get/move_history/get_all/epc

--- controllers/controllers_move_history.py
+++ controllers/controllers_move_history.py
@@ -2,10 +2,12 @@
 from odoo import http
 
 class ControllerMoveHistory(http.Controller):
+   
 
-    @http.route('/parking/get/move_history/get_all', website=False,csrf=False, type='json', methods=['GET'], auth='public')
+    @http.route('/parking/get/move_history/get_all/epc', website=False,csrf=False, type='json', methods=['POST'], auth='public')
     def get_all_move_history(self, **kw):
         move_histories = http.request.env['stock.move.line'].sudo().search_read(
+            domain=[('lot_name', '=', kw['sEPC']),('date','=', fields.datetime.now())],
             fields=['date','lot_name', 'reference','location_id','location_dest_id'],
         )
         return move_histories
