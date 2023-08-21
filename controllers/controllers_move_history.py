e21d13f65c02da4f91fce13d3a0bfb13583572ac
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-21 09:00:39 +0700

    add api /parking/get/move_history/get_all

--- controllers/controllers_move_history.py
+++ controllers/controllers_move_history.py
@@ -2,9 +2,8 @@
 from odoo import http
 
 class ControllerMoveHistory(http.Controller):
-   
 
-    @http.route('/parking/get/move_history/get_all', auth='public', website=False,csrf=False, type='json', methods=['GET'], auth='public')
+    @http.route('/parking/get/move_history/get_all', website=False,csrf=False, type='json', methods=['GET'], auth='public')
     def get_all_move_history(self, **kw):
         move_histories = self.env['stock.move.line'].sudo().search_read(
             fields=['date','lot_name', 'reference','location_id','location_dest_id'],
