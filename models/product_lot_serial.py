ff0c6e97fe18b2388cf2045996a2b4ee3a747a63
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-22 14:25:26 +0700

    add fields

--- models/product_lot_serial.py
+++ models/product_lot_serial.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 class stoct_lot(models.Model):
     _inherit = 'stock.lot'
-    location_id  = fields.Many2one('stock.location', string="Vị Trí",default=55)
+    location_id  = fields.Many2one('stock.location', string="Vị Trí")
     state = fields.Char(string="Trạng Thái")
 
