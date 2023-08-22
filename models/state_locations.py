777251c071e6f4dcc969e8f13aaecae587b45997
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-08-22 15:38:01 +0700

    add fields

--- models/state_locations.py
+++ models/state_locations.py
@@ -7,6 +7,6 @@ class state_locations(models.Model):
     state = fields.Selection(
         [('empty', 'Trống'), ('full', 'Đầy')], string="Trạng thái", default='empty')
     product_id_name = fields.Many2one(
-        'product.product', string="Biển số", default=211)
+        'product.product', string="Biển số", default=209)
     # product_name  = fields.Many2one('stock.lot', string="Biển số",relation="product_id")
     lot_name = fields.Many2one('stock.lot', string="Mã định danh", default=63)
