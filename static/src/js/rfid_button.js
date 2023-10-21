4c3dc69f7983502e9402c623f071a61192c27910
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-10-21 16:52:48 +0700

    ad

--- static/src/js/rfid_button.js
+++ static/src/js/rfid_button.js
@@ -1,6 +1,6 @@
 odoo.define("parking_odoo.RFID_button", function (require, factory) {
   "use strict";
   console.log("rfid_button.js load");
-  const wsUri = "ws://127.0.0.1:62536/";
+  const wsUri = "ws://localhost:62536/";
   const websocket = new WebSocket(wsUri);
 });
