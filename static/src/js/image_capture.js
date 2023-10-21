66a9b31ac0e437fd71563c3eef7bbbe458750192
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2023-10-21 11:20:27 +0700

    add

--- static/src/js/image_capture.js
+++ static/src/js/image_capture.js
@@ -126,11 +126,14 @@ export class ImageCapture extends Component {
     var save_image = document.getElementById("save_image"+this.props.name);
     var image = document.getElementById("image"+this.props.name);
     const wsUri = "ws://127.0.0.1:62536/";
+    console.log(wsUri)
     const websocket = new WebSocket(wsUri);
-
+    console.log(websocket)
     websocket.onopen = (e) => {
       console.log("Da Mo")
     };
+
+   
     var context = canvas.getContext("2d");
 
     save_image.classList.remove("d-none");
